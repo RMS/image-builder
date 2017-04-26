ff60098fe63f0ba99ec62753cdc0d81a02e0e30b
Brandon Geffe <brandon@rms.com>
2017-04-26 07:46:05 -0600

    when in rome

--- circleci-provision-scripts/rms.sh
+++ circleci-provision-scripts/rms.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 function install_rms() {
-  pyenv global 3.5.2
-  pip3 install lxml
-  pip3 install marathon
-  pip3 install dcos==0.4.14
-  pip3 install Jinja2==2.8
-  pip3 install msrestazure==0.4.1
-  pip3 install paramiko==2.0.1
-  pip3 install scp==0.10.2
-  pip3 install PyYAML==3.12
-  pip3 install azure==2.0.0rc5
-  pip3 install consul_kv
-  pip3 install keyring
-  pip3 install requests
+  (cat <<'EOF'
+source ~/.circlerc
+pyenv global 3.5.2
+pip install lxml
+pip install marathon
+pip install dcos==0.4.14
+pip install Jinja2==2.8
+pip install msrestazure==0.4.1
+pip install paramiko==2.0.1
+pip install scp==0.10.2
+pip install PyYAML==3.12
+pip install azure==2.0.0rc5
+pip install consul_kv
+pip install keyring
+pip install requests
+EOF
+  ) | as_user bash
 }
