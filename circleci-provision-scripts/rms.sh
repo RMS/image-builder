034b1b5608b224938d944b9343fc43250316125f
Brandon Geffe <brandon@rms.com>
2017-04-27 13:50:35 -0600

    hmm

--- circleci-provision-scripts/rms.sh
+++ circleci-provision-scripts/rms.sh
@@ -15,6 +15,29 @@ pip install azure==2.0.0rc5
 pip install consul_kv
 pip install keyring
 pip install requests
+apt-get install jq
+apt-get install sshuttle
+echo "\n" | ssh-add /home/ubuntu/.ssh/id_*
+wget https://releases.hashicorp.com/consul/0.7.3/consul_0.7.3_linux_amd64.zip
+wget https://releases.hashicorp.com/vault/0.6.4/vault_0.6.4_linux_amd64.zip
+unzip consul_0.7.3_linux_amd64.zip
+unzip vault_0.6.4_linux_amd64.zip
+sudo mv consul /usr/local/bin/consul
+sudo mv vault /usr/local/bin/vault
+consul agent -dev &
+sleep 20s
+vault server -dev -dev-root-token-id="test-token" &
+sleep 10s
+consul kv put system/domain azure.rmsonecloud.net
+consul kv put system/environment test
+vault write secret/postgres/user value=postgres
+vault write secret/postgres/password value=password
+vault write secret/2/postgres/user value=postgres2
+vault policy-write rms-policy rms.hcl
+curl -X POST -H "X-Vault-Token:test-token" -d '{"type":"approle"}' http://127.0.0.1:8200/v1/sys/auth/approle
+curl -X POST -H "X-Vault-Token:test-token" -d '{"policies":"rms-policy"}' http://127.0.0.1:8200/v1/auth/approle/role/rms
+echo "export VAULT_SECRET_ID=$(curl --silent -X POST -H "X-Vault-Token:test-token" http://127.0.0.1:8200/v1/auth/approle/role/rms/secret-id | jq -r .data.secret_id)" >> ~/.circlerc
+echo "export VAULT_ROLE_ID=$(curl --silent -X GET -H "X-Vault-Token:test-token" http://127.0.0.1:8200/v1/auth/approle/role/rms/role-id | jq -r .data.role_id)" >> ~/.circlerc
 EOF
   ) | as_user bash
 }
