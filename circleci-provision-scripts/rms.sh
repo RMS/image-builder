4fa5e8430145337d86a7c2ede479cde74b69de23
Brandon Geffe <brandon@rms.com>
2017-04-27 22:25:49 -0600

    add hcl

--- circleci-provision-scripts/rms.sh
+++ circleci-provision-scripts/rms.sh
@@ -32,7 +32,7 @@ export VAULT_ADDR=http://127.0.0.1:8200/
 sudo -E vault write secret/postgres/user value=postgres
 sudo -E vault write secret/postgres/password value=password
 sudo -E vault write secret/2/postgres/user value=postgres2
-sudo -E vault policy-write rms-policy rms.hcl
+sudo -E vault policy-write rms-policy /opt/rms.hcl
 curl -X POST -H "X-Vault-Token:test-token" -d '{"type":"approle"}' http://127.0.0.1:8200/v1/sys/auth/approle
 curl -X POST -H "X-Vault-Token:test-token" -d '{"policies":"rms-policy"}' http://127.0.0.1:8200/v1/auth/approle/role/rms
 sudo echo "export VAULT_SECRET_ID=$(export VAULT_ADDR=http://127.0.0.1:8200/ && curl --silent -X POST -H "X-Vault-Token:test-token" http://127.0.0.1:8200/v1/auth/approle/role/rms/secret-id | jq -r .data.secret_id)" >> ~/.circlerc
