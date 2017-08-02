98a03e5526a2f10fbeb5e6b611d2382e6db042b4
Kevin Booth <kbooth@rms.com>
2017-08-02 16:31:59 -0400

    adding python deps back in, we can discuss later

--- circleci-provision-scripts/rms.sh
+++ circleci-provision-scripts/rms.sh
@@ -3,19 +3,18 @@ function install_rms() {
   (cat <<'EOF'
 source ~/.circlerc
 pyenv global 3.6.1
-# pip install lxml
-# pip install marathon
-# pip install dcos==0.4.14
-# pip install Jinja2==2.8
-# pip install msrestazure==0.4.1
-# pip install paramiko==2.0.1
-# pip install scp==0.10.2
-# pip install PyYAML==3.12
-# pip install azure==2.0.0rc5
-# pip install consul_kv
-# pip install keyring
-# pip install hvac
-# pip install requests
+pip install lxml
+pip install marathon
+pip install dcos==0.4.14
+pip install Jinja2==2.8
+pip install msrestazure==0.4.1
+pip install paramiko==2.0.1
+pip install scp==0.10.2
+pip install PyYAML==3.12
+pip install azure==2.0.0rc5
+pip install consul_kv
+pip install keyring
+pip install requests
 sudo apt-get install jq
 sudo wget https://releases.hashicorp.com/consul/0.7.3/consul_0.7.3_linux_amd64.zip
 sudo wget https://releases.hashicorp.com/vault/0.6.4/vault_0.6.4_linux_amd64.zip
