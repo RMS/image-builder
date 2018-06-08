d5bab3e0eeebab10bfd8fc1a27d4affed131141c
Kevin Booth <kbooth@cardinalsolutions.com>
2018-06-08 16:25:06 -0400

    cleaning up scripts

--- build.sh
+++ build.sh
@@ -21,3 +21,6 @@ sudo docker build ${NO_CACHE} --build-arg IMAGE_TAG=buildbox \
 -t ${PUSH_REPO}/buildbox:${TAG} \
 -f targets/ubuntu-14.04-thin/Dockerfile \
 .
+
+
+sudo docker push ${PUSH_REPO}/buildbox:${TAG}
