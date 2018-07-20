9b6594e0528027287602a4bac8c8541d96c1d4a6
kmyers <kmyers@rms.com>
2018-07-20 15:57:47 -0500

    Try running as root instead

--- tests/test_docker_image.py
+++ tests/test_docker_image.py
@@ -12,7 +12,7 @@ def host():
         'sudo docker run -d --name test -e GIT_CRYPT_KEY={} eastus-artifactory.azure.rmsonecloud.net:6001/buildbox:{} sleep 90'.format(os.environ['GIT_CRYPT_KEY'], os.environ['TAG']).split()).decode().strip()
     # Note I am setting it to use the `ubuntu` user specifically below because the default user is `root`
     # TODO Should we change the image to just default to the `ubuntu` user?
-    yield testinfra.get_host("docker://ubuntu@" + docker_id, sudo=True)
+    yield testinfra.get_host("docker://root@" + docker_id)
     subprocess.check_call(['sudo', 'docker', 'rm', '-f', docker_id])
 
 
