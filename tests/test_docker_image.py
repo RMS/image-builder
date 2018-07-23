79b76006dc5ce1a703860f3d5ed81751a1ed2cc0
kmyers <kmyers@rms.com>
2018-07-23 13:32:34 -0500

    Simulate a test failure

--- tests/test_docker_image.py
+++ tests/test_docker_image.py
@@ -7,18 +7,16 @@ import testinfra
 
 @pytest.fixture(scope='session')
 def host():
-    # TODO Pass in the image name/ID that was just built
     docker_id = subprocess.check_output(
         'sudo docker run -d --name test -e GIT_CRYPT_KEY={} eastus-artifactory.azure.rmsonecloud.net:6001/buildbox:{} sleep 90'.format(os.environ['GIT_CRYPT_KEY'], os.environ['TAG']).split()).decode().strip()
     # Note I am setting it to use the `ubuntu` user specifically below because the default user is `root`
-    # TODO Should we change the image to just default to the `ubuntu` user?
     yield testinfra.get_host("docker://ubuntu@" + docker_id)
     subprocess.check_call(['sudo', 'docker', 'rm', '-f', docker_id])
 
 
 def test_default_user(host):
     user = host.user()
-    assert user.name == 'ubuntu'
+    assert user.name == 'fail'
 
 
 def test_rms_one_deployment_directory_permissions(host):
