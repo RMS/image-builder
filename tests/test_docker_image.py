3b7f6bc02a73f0c7857ba12108406fd8e714b207
kmyers <kmyers@rms.com>
2018-07-23 13:53:21 -0500

    For clarity use CIRCLE_BRANCH var instead of TAG

--- tests/test_docker_image.py
+++ tests/test_docker_image.py
@@ -8,7 +8,7 @@ import testinfra
 @pytest.fixture(scope='session')
 def host():
     docker_id = subprocess.check_output(
-        'sudo docker run -d --name test -e GIT_CRYPT_KEY={} eastus-artifactory.azure.rmsonecloud.net:6001/buildbox:{} sleep 90'.format(os.environ['GIT_CRYPT_KEY'], os.environ['TAG']).split()).decode().strip()
+        'sudo docker run -d --name test -e GIT_CRYPT_KEY={} eastus-artifactory.azure.rmsonecloud.net:6001/buildbox:{} sleep 90'.format(os.environ['GIT_CRYPT_KEY'], os.environ['CIRCLE_BRANCH']).split()).decode().strip()
     # Note I am setting it to use the `ubuntu` user specifically below because the default user is `root`
     yield testinfra.get_host("docker://ubuntu@" + docker_id)
     subprocess.check_call(['sudo', 'docker', 'rm', '-f', docker_id])
